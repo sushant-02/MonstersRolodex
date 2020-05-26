5de608be79879b4be3c76630fc732c2053f40c66
Sushant Pandey <skp000002@gmail.com>
2020-05-26 10:00:43 +0530

    Changed some code

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
     .then(users => this.setState({ monsters: users}));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter( monster =>
@@ -30,7 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value })}/>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
         <CardList monsters= {filteredMonsters}/>
       </div>
     );
