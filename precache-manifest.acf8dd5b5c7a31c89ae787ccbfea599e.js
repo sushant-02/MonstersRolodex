39018d3f32a36b32b2f65649cbbeebdfa0bd2b94
Sushant Pandey <skp000002@gmail.com>
2020-05-26 10:21:31 +0530

    Updates

---
+++ precache-manifest.acf8dd5b5c7a31c89ae787ccbfea599e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df44841a07ee725a05673881807886d3",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "ad8fc202833bb35e6c54",
+    "url": "/MonstersRolodex/static/css/main.57739dde.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8fc202833bb35e6c54",
+    "url": "/MonstersRolodex/static/js/main.172d64a6.chunk.js"
+  },
+  {
+    "revision": "0356c89559ba280291dc",
+    "url": "/MonstersRolodex/static/js/runtime-main.ee30fdb1.js"
+  }
+]);
